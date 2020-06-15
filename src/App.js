da0e1434913f4ef96399d3600b1f97f7de7fc8e4
Vasily Kurdyaev <kurdyaev.exceedteam@gmail.com>
2020-06-15 18:33:22 +0300

    Add scroll using scroll-into-view-if-needed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import scrollIntoView from "scroll-into-view-if-needed";
 
-function App() {
+export default function App() {
+  const [messages, setMessages] = useState([
+    "New message",
+    "New message",
+    "New message",
+    "New message",
+  ]);
+
+  const [autoAddMessages, setAutoAddMessages] = useState(true);
+  const messageElements = messages.map((msg, index) => {
+    const className =
+      index === messages.length - 1 ? "msg last-message" : "msg";
+    return (
+      <div key={index} className={className}>
+        {`${msg} [${index}]`}
+      </div>
+    );
+  });
+
+  useEffect(() => {
+    if (autoAddMessages) {
+      const timer = setTimeout(() => {
+        setMessages((prev) => [...prev, "New message"]);
+      }, 1500);
+      return () => clearTimeout(timer);
+    }
+  }, [messages, autoAddMessages]);
+
+  useEffect(() => {
+    if (messages.length) {
+      const elements = document.getElementsByClassName("last-message");
+      const element = elements[0];
+
+      scrollIntoView(element, {
+        scrollMode: "if-needed",
+        behavior: "smooth",
+      });
+    }
+  }, [messages]);
+
+  const autoButtonText = autoAddMessages
+    ? "Disable auto messages"
+    : "Enable auto messages";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="message-container">
+        <header className="header">scroll-into-view-if-needed example</header>
+        <div className="container">{messageElements}</div>
+      </div>
+      <div className="controls">
+        <button onClick={() => setMessages((prev) => [...prev, "New message"])}>
+          Add message
+        </button>
+        <button onClick={() => setMessages([])}>Clear messages</button>
+        <button onClick={() => setAutoAddMessages((prev) => !prev)}>
+          {autoButtonText}
+        </button>
+      </div>
     </div>
   );
 }
-
-export default App;
